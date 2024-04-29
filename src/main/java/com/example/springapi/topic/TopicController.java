d7c45e66d863af89d9e278676a5eb1895f802e49
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-04-29 14:32:00 +0330

    added update method

--- src/main/java/com/example/springapi/topic/TopicController.java
+++ src/main/java/com/example/springapi/topic/TopicController.java
@@ -28,4 +28,9 @@ public class TopicController {
         topicService.addTopic(topic);
     }
 
+    @RequestMapping(method = RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+        topicService.updateTopic(id, topic);
+    }
+
 }
