358ec40d1ba253707e6fc7aa0de49a716983af18
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-05-01 12:32:04 +0330

    added testaddTopic to APITest

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,11 +1,39 @@
 import com.example.springapi.topic.Topic;
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
 
 
 public class APITests {
 
     @Test
-    void TestaddTopic(){
+    public void TestaddTopic(){
+
+//        baseURI = "localhost:8080";
+//        Map<String, Object> map = new HashMap<String, Object>();
+//
+//        map.put("id", "testNG");
+//        map.put("name", "testNG title");
+//        map.put("description", "testNG description");
+//
+        JSONObject request = new JSONObject();
+
+        request.put("id","testNG");
+        request.put("name","testNG title");
+        request.put("description","testNG description");
+
+        given().
+            contentType(ContentType.JSON).
+        accept(ContentType.JSON).
+            body(request.toJSONString()).
+        when().
+            post("http://localhost:8080/topics").
+        then().
+            statusCode(200);
+
 
 
     }
