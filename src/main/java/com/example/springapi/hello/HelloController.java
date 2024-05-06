f1e2829e25e57c7eb2c54c9f4ec2bf457098fed5
Mohammad Mahdi Sabet <metisbt@gmail.com>
2024-05-06 08:25:22 +0330

    update

--- src/main/java/com/example/springapi/hello/HelloController.java
+++ src/main/java/com/example/springapi/hello/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springapi.hello;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
